1fb6ccbc376f0a60b1b7d289320cc7a4598c2895
shadow6444 <shadowgarden644@gmail.com>
2025-06-22 18:11:26 +0500

    Initialize workspace
    First day of initializing the project folder. Made the project folder to develop the application

---
+++ src/com/todo/ToDoApplication.java
@@ -0,0 +1,43 @@
+package com.todo;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ToDoApplication {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ToDoApplication window = new ToDoApplication();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ToDoApplication() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ToDoApplication {
+	requires java.desktop;
+}
